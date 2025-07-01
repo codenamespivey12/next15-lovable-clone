215174490ce368a834635f90795eaba6227442b1
Jorge Matos <jisapjisap@gmail.com>
2025-07-01 18:06:54 +0200

    Add @e2b/code-interpreter integration: Update package.json and package-lock.json to include the new dependency. Enhance helloWorld function to create and retrieve sandbox IDs, and add utility function for sandbox connection.

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,11 +1,17 @@
 import { inngest } from "./client";
 import { openai, createAgent, gemini } from "@inngest/agent-kit";
-
+import  { Sandbox } from "@e2b/code-interpreter"
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
+
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("lovableclone - test4");
+      return sandbox.sandboxId;
+    })
 
     const codeAgent = createAgent({
       name: "code-agent",
@@ -16,9 +22,15 @@ export const helloWorld = inngest.createFunction(
     const { output } = await codeAgent.run(
       `Write the following snippet: ${event.data.value}`,
     )
-    
-    console.log(output)
 
-    return { output };
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`
+    })
+    
+    console.log(output, sandboxUrl)
+
+    return { output, sandboxUrl };
   },
 );
