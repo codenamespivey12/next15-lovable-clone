14ab2a1e607ab9f9d950d974019a20522e4747d8
Jorge Matos <jisapjisap@gmail.com>
2025-07-02 18:41:47 +0200

    Enhance helloWorld function: Add terminal tool integration for command execution within the sandbox environment. Refactor sandbox ID retrieval and improve error handling for command execution results.

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,28 +1,62 @@
 import { inngest } from "./client";
-import { openai, createAgent, gemini } from "@inngest/agent-kit";
+import { openai, createAgent, gemini, createTool } from "@inngest/agent-kit";
 import  { Sandbox } from "@e2b/code-interpreter"
 import { getSandbox } from "./utils";
+import { z } from "zod";
 
 export const helloWorld = inngest.createFunction(
+  
   { id: "hello-world" },
   { event: "test/hello.world" },
+
   async ({ event, step }) => {
-
-    const codeAgent = createAgent({
-      name: "code-agent",
-      system: "You are an expert next.js developer. You write readable, maintainable, and testable code. You write simple Next.js & React snippets.",
-      model: gemini({ model: "gemini-1.5-flash", apiKey: process.env.GEMINI_API_KEY }),
-    });
-
-    const { output } = await codeAgent.run(
-      `Write the following snippet: ${event.data.value}`,
-    )
 
     const sandboxId = await step.run("get-sandbox-id", async () => {
       const sandbox = await Sandbox.create("lovableclone-test9");
       //await sandbox.setTimeout(60)
       return sandbox.sandboxId;
     })
+
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system: "You are an expert next.js developer. You write readable, maintainable, and testable code. You write simple Next.js & React snippets.",
+      model: gemini({ model: "gemini-1.5-flash", apiKey: process.env.GEMINI_API_KEY }),
+      tools: [
+        createTool({
+          name: "terminal",
+          description: "Use the terminal to run commands",
+          parameters: z.object({
+            command: z.string(),
+          }),
+          handler: async({ command }, { step }) => {
+            return await step?.run("terminal", async() => {
+              const buffers = {stdout: "", stderr: ""}
+              try {
+                const sandbox = await getSandbox(sandboxId);
+                const result = await sandbox.commands.run(command, {
+                  onStdout: (data:string) => {
+                    buffers.stdout += data
+                  },
+                  onStderr: (data:string) => {
+                    buffers.stderr += data
+                  },
+                });
+                return result.stdout
+              }catch(e){
+                console.error(
+                  `Command failed: ${e} \nstdout: ${buffers.stdout}\nstderror: ${buffers.stderr}`
+                );
+                return `Command failed: ${e} \nstdout: ${buffers.stdout}\nstderror: ${buffers.stderr}`  
+              }
+            })
+          }
+        })
+      ]
+    });
+
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`,
+    )
 
     const sandboxUrl = await step.run("get-sandbox-url", async () => {
       const sandbox = await getSandbox(sandboxId);
