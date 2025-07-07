29d1ce98b980c390837034e8f4adfcda08b50212
Jorge Matos <jisapjisap@gmail.com>
2025-07-07 18:01:01 +0200

    Add tabbed interface to ProjectView for code and preview display: Introduce Tabs component to allow users to switch between a preview of the active fragment and a code view. Enhance UI with buttons for upgrading and improve layout structure for better user experience.

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -7,6 +7,10 @@ import { Suspense, useState } from "react";
 import { Fragment } from "@/generated/prisma";
 import { ProjectHeader } from "../components/ProjectHeader";
 import { FragmentWeb } from "../components/fragment-web";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { CodeIcon, Crown, CrownIcon, EyeIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 
 interface Props {
@@ -16,10 +20,12 @@ interface Props {
 export const ProjectView = ({ projectId }: Props) => {
 
   const [activeFragment, setActiveFragment] = useState<Fragment | null>(null);
+  const [tabState, setTabState] = useState<"preview" | "code">("preview");
 
   return (
     <div className="h-screen">
       <ResizablePanelGroup direction="horizontal">
+        {/* Panel de mensajes */}
         <ResizablePanel
           defaultSize={35}
           minSize={20}
@@ -41,11 +47,51 @@ export const ProjectView = ({ projectId }: Props) => {
 
         <ResizableHandle withHandle />
 
+      {/* Panel de código */}
        <ResizablePanel
           defaultSize={65}
           minSize={50}
        >
-          {!!activeFragment && <FragmentWeb data={activeFragment} />}
+          <Tabs
+            className="h-full gap-y-0"
+            defaultValue="preview"
+            value={tabState}
+            onValueChange={(value) => setTabState(value as "preview" | "code")}
+          >
+            <div className="w-full flex items-center p-2 border-b gapx-2">
+              <TabsList className="h-8 p-0 border rounded-md">
+                <TabsTrigger
+                  value="preview"
+                  className="rounded-md"
+                >
+                  <EyeIcon /> <span>Demo</span>
+                </TabsTrigger>
+                <TabsTrigger
+                  value="code"
+                  className="rounded-md"
+                >
+                  <CodeIcon /> <span>Code</span>
+                </TabsTrigger>
+              </TabsList>
+
+              <div className="ml-auto flex items-center gap-x-2">
+                <Button asChild size="sm" variant="default">
+                  <Link href="/pricing">
+                    <CrownIcon /> Upgrade
+                  </Link>
+                </Button>
+              </div>
+            </div>
+
+            <TabsContent value="preview">
+                {!!activeFragment && <FragmentWeb data={activeFragment} />}
+            </TabsContent>
+            <TabsContent value="code">
+              <p>
+                // TODO: Code view
+              </p>
+            </TabsContent>
+          </Tabs>
         </ResizablePanel>
       </ResizablePanelGroup>
     </div>
