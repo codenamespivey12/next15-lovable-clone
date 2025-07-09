6553c1c70a49ce551ccecd511e745ad1dc398c15
Jorge Matos <jisapjisap@gmail.com>
2025-07-09 16:24:28 +0200

    Fix layout issues and add ProjectsList component: Corrected a typo in the layout class names and adjusted padding in the home page. Introduced a new ProjectsList component to display saved projects, enhancing the user interface and project management experience.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,6 +3,7 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input";
 import { ProjectForm } from "@/modules/home/ui/components/project-form";
+import ProjectsList from "@/modules/home/ui/components/projects-list";
 import { useTRPC } from "@/trpc/client";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import Image from "next/image";
@@ -12,41 +13,9 @@ import { toast } from "sonner";
 
 const Page = () => {
 
-  // const router = useRouter()
-
-  // const [value, setValue] = useState("")
-
-  // const trpc = useTRPC();
-  
-  // const createProject = useMutation(trpc.projects.create.mutationOptions({
-  //   onError: (error) => {
-  //     toast.error(error.message)
-  //   },
-  //   onSuccess: (data) => {
-  //     toast.success("Project created successfully")
-  //     router.push(`/projects/${data.id}`)
-  //   }
-  // }));
-
-
-
-  // return (
-  //   <div className="h-screen w-screen flex items-center justify-center">
-  //     <div className="max-w-7xl mx-auto flex items-center flex-col gap-y-4 justify-center">
-  //       <Input value={ value } onChange={ (e) => setValue(e.target.value) }/>
-  //       <Button 
-  //         disabled={createProject.isPending}
-  //         onClick={() => createProject.mutate({ value: value})}
-  //       >
-  //         Submit
-  //       </Button>
-  //     </div>
-  //   </div>
-  //)
-
   return (
     <div className="flex flex-col max-w-5xl mx-auto w-full">
-      <section className="space-y-6 py-[16vh] 2xl:py-48">
+      <section className="space-y-6 py-[16vh] 2xl:py-38">
         <div className="flex flex-col items-center">
           <Image 
             src="/logo.svg"
@@ -66,6 +35,8 @@ const Page = () => {
           <ProjectForm />
         </div>
       </section>
+
+      <ProjectsList />
     </div>
   )
 }
