c7403e714d667d6517b229e4e5dc0bd5719cb29d
codenamespivey12 <codenamespivey@sixtyoneeightyai.com>
2025-07-11 07:33:15 -0500

    Update navbar.tsx

--- src/modules/home/ui/components/navbar.tsx
+++ src/modules/home/ui/components/navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = () => {
             width={24} 
             height={24} 
           />
-          <span className="font-semibold text-lg">Lovable</span>
+          <span className="font-semibold text-lg">sixtyoneeighty</span>
         </Link>
 
         <div className="flex gap-2">
